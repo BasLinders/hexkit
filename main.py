f662684978b339814e43a164c0a2d6e0b17a9e7b
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2026-06-01 16:12:10 +0200

    Update version number to v1.4.1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.sidebar.image(logo_url, width="stretch")
 
 # st.title("HEXKIT")
 # st.image(logo_url)
-st.write("### <span style='color: orange;'>v1.4</span>", unsafe_allow_html=True)
+st.write("### <span style='color: orange;'>v1.4.1</span>", unsafe_allow_html=True)
 st.write("""
 This is the main page for the Happy Horizon Experimentation Toolkit. You can navigate to individual apps using the sidebar.
 
